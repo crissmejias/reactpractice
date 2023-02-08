7a59cb82e25fda394e68447a77240aa6d72d7321
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2023-02-08 13:23:11 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const App = () => {
+  return <div>Hola mundo!</div>;
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./components/App";
+import React from "react";
+import ReactDOM from "react-dom";
+const rootElement = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
